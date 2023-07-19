6a68d08b6a8548f2291cd3b58d34551df7c64ae5
Lee <Lee>
2023-07-19 17:06:40 +0900

    test4

--- js/main.js
+++ js/main.js
@@ -194,7 +194,6 @@ const getDateDiff = (d1, d2) => {
 				$('.number').each(function(){
 					var $this = $(this),
 						num = $this.data('number');
-						console.log(num);
 					$this.animateNumber(
 					  {
 					    number: num,
